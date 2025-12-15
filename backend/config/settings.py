763d35770a99032dcbf7f9dee46ab3377270ce42
rvndudz <rvndudz77@gmail.com>
2025-12-15 13:18:16 +0530

    Add CORS settings to allow requests from frontend URLs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -124,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",  # Your frontend URL
+    "http://127.0.0.1:8080",
+]
+
+CORS_ALLOW_CREDENTIALS = True
