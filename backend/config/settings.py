44f06e8245f82399d820c076faca66736633e282
rvndudz <rvndudz77@gmail.com>
2025-12-15 13:19:02 +0530

    Add REST framework authentication and permission settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,3 +131,12 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
